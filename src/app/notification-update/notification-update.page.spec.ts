b6da5c45ed525d696d599896e290057b0a261241
tsheling1993 <tashioongchuk@gmail.com>
2019-11-09 23:34:25 +0600

    update or edit notification

---
+++ src/app/notification-update/notification-update.page.spec.ts
@@ -0,0 +1,27 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NotificationUpdatePage } from './notification-update.page';
+
+describe('NotificationUpdatePage', () => {
+  let component: NotificationUpdatePage;
+  let fixture: ComponentFixture<NotificationUpdatePage>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NotificationUpdatePage ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NotificationUpdatePage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
