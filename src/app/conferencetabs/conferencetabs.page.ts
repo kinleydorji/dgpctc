7d798ebeb535dee5f7dcbdb0ee3591aa77eeac39
karmaherrera <dorjikarma99@gmail.com>
2019-11-26 21:21:06 +0600

    result open and close done

--- src/app/conferencetabs/conferencetabs.page.ts
+++ src/app/conferencetabs/conferencetabs.page.ts
@@ -10,6 +10,7 @@ import { Storage } from '@ionic/storage';
 })
 export class ConferencetabsPage implements OnInit {
   private voteOption;
+  private resultOption;
   private hall="";
   private selectedHall;
   constructor(private router: Router, private afs: AngularFirestore, private route: ActivatedRoute, private storage: Storage) { 
@@ -21,6 +22,11 @@ export class ConferencetabsPage implements OnInit {
     this.router.navigate(['castvote']);
   }
 
+  result()
+  {
+    this.router.navigate(['result']);
+  }
+
   ngOnInit() {
     this.storage.get('hall').then((hall) => {
       this.selectedHall = hall;
@@ -29,8 +35,14 @@ export class ConferencetabsPage implements OnInit {
     let results;
     this.afs.collection('t_overallStatus').valueChanges().subscribe(result => {
       results = result;
-      this.voteOption = results[0].votingStatus;
-      console.log("details", results[0].votingStatus);
+      this.voteOption = results[1].votingStatus;
+      console.log("details", results[1].votingStatus);
+    })
+
+    this.afs.collection('t_overallStatus').valueChanges().subscribe(result => {
+      results = result;
+      this.resultOption = results[0].resultStatus;
+      console.log("details", results[0].resultStatus);
     })
   }
 
--- src/app/result/result.page.ts
+++ src/app/result/result.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-result',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultPage implements OnInit {
 
-  constructor() { }
+  private voteResult;
+  private cardColors =[];
+  private temp;
+  constructor(private afs: AngularFirestore) { 
+
+    this.cardColors = ["#b8dfca", "#fbf8e6", "#e6e6fa", "#fff7f6", "#e8f4f8"];
+  }
+
+
 
   ngOnInit() {
+    this.afs.collection('t_poll').valueChanges().subscribe(result => {
+      this.voteResult = result;
+      console.log("details", this.voteResult);
+      for(let i = 0; i < this.voteResult.length; i++)
+      {
+        for(let j = i + 1; j < this.voteResult.length; j++)
+        {
+          if(this.voteResult[i].voteCount < this.voteResult[j].voteCount)
+          {
+            this.temp = this.voteResult[i];
+            this.voteResult[i] = this.voteResult[j];
+            this.voteResult[j] = this.temp;
+          }
+        }
+      }
+
+      console.log("Sorted : ", this.voteResult);
+    })
   }
+
+
 
 }
